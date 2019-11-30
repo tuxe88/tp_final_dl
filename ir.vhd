21b9ba67341885705f46e613bccbe168e1b5223b
ivanszysz <39999076+ivanszysz@users.noreply.github.com>
2019-11-30 18:24:09 -0300

    Add files via upload

--- ir.vhd
+++ ir.vhd
@@ -7,16 +7,17 @@ entity ir is
    port (
          clk, rst: in std_logic;
          input: in std_logic_vector(15 downto 0);
-         output: out std_logic_vector(15 downto 0);
-
+         output: out std_logic_vector(15 downto 0));
 end ir;
+
+architecture beh_proc of ir is 
 
 begin
    
    pir: process (clk, rst)
    begin
       if rst='1' then
-         output <= x'0000';
+         output <= x"0000";
       elsif (clk'event and clk='1') then
          output <= input;
       end if;   
